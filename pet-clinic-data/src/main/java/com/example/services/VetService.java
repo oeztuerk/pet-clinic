d53c587e57c0136c9489b6842f415413f810dbf1
Sefa Öztürk <sefa.oeztuerk@porscheinformatik.com>
2021-09-28 15:06:29 +0200

    Make Common Interface and inherit

--- pet-clinic-data/src/main/java/com/example/services/VetService.java
+++ pet-clinic-data/src/main/java/com/example/services/VetService.java
@@ -2,11 +2,6 @@ package com.example.services;
 
 import com.example.model.Vet;
 
-import java.util.Set;
-
-public interface VetService
+public interface VetService extends CrudService<Vet, Long>
 {
-    Vet findById(Long id);
-    Vet save(Vet owner);
-    Set<Vet> findAll();
 }
