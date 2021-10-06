6a3fccb54f40f601cc2780ec23fa5ef83e516f38
Sefa Öztürk <sefa.oeztuerk@porscheinformatik.com>
2021-10-06 14:02:45 +0200

    closes #27

---
+++ pet-clinic-data/src/main/java/com/example/model/Speciality.java
@@ -0,0 +1,16 @@
+package com.example.model;
+
+public class Speciality extends BaseEntity
+{
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    private String description;
+}
